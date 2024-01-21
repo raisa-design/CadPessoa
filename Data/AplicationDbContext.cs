c5e96ec8a58e5dfe7fa6ec660582fc76bbb01f57
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-01-21 04:30:14 -0300

    Configurando contexto de dados

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadPessoa.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
