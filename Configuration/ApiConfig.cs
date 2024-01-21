8556b732dbcb3ca5dd1388cc6887992bf7a5fcc7
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-01-21 04:42:12 -0300

    adicionando ApiConfig

---
+++ Configuration/ApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CadPessoa.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("Development");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
